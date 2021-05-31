f87bb3c1d175b9af762e4bbb6496bf729414c7e8
RavisAzure <me_mohammadian@yahoo.com>
2021-05-31 16:24:10 +0300

    1

--- pages/Components/nav_bar/NavBar.component.jsx
+++ pages/Components/nav_bar/NavBar.component.jsx
@@ -18,7 +18,7 @@ class NavBar extends React.Component {
             <nav className={styles.navVar}>
                 <div className={styles['nav-text-detail']}>
                     <div>
-                        <a href="/ویلا/intro" className="font-size-10 btn-outlined btn-nav-hosting">میزبان شوید</a>
+                        <a href="/ویلا/intro" className={`font-size-10 btn-outlined ${styles['btn-nav-hosting']}`}>میزبان شوید</a>
                     </div>
                     <div className="font-size-12">
                         <div className={styles['nav-detail-first-line']}>
@@ -61,25 +61,25 @@ class NavBar extends React.Component {
                     <ul className={styles['navbar-items']}>
                         <li>
                             <a href="/">
-                                <i className="kilo-font icon-plane-departure nav-icon rotate-y-180"></i>
+                                <i className={`kilo-font icon-plane-departure  ${styles['nav-icon']} rotate-y-180`}></i>
                                 بلیط هواپیما
                             </a>
                         </li>
                         <li>
                             <a href="/ویلا">
-                                <i className="kilo-font icon-villa nav-icon"></i>
+                                <i className={`kilo-font icon-villa ${styles['nav-icon']} `}></i>
                                 اقامتگاه
                             </a>
                         </li>
                         <li>
                             <a href="#">
-                                <i className="kilo-font icon-blog nav-icon"></i>
+                                <i className={`kilo-font icon-blog ${styles['nav-icon']} `}></i>
                                 بلاگ
                             </a>
                         </li>
                         <li>
                             <a href="/بلیط-هواپیما/order">
-                                <i className="kilo-font icon-refrence nav-icon"></i>
+                                <i className={`kilo-font icon-refrence ${styles['nav-icon']} `}></i>
                                 پیگیری خرید
                             </a>
                         </li>
