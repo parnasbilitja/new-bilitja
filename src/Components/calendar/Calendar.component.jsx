c176b7da8629b1a758f08742a40941d570d751a0
RavisAzure <83583293+RavisAzure@users.noreply.github.com>
2021-07-10 20:58:30 +0300

    1

--- src/Components/calendar/Calendar.component.jsx
+++ src/Components/calendar/Calendar.component.jsx
@@ -25,10 +25,10 @@ class CalendarComponent extends React.Component {
                
 
                 {
-                    this.state.typeOfCalendar == "JAL" ?
+                    //this.state.typeOfCalendar == "JAL" ?
                         
-                        <JalaliDays setDate={this.props.setDate} closePopUpCalendar={this.props.closePopUpCalendar}/>
-                        :
+                        // <JalaliDays setDate={this.props.setDate} closePopUpCalendar={this.props.closePopUpCalendar}/>
+                        // :
                         <GarigorianDays setDate={this.props.setDate} closePopUpCalendar={this.props.closePopUpCalendar}/>
                 }
                  <div className="rtl text-right">
