1372c1cfda57573a2d393d0a2dbc7569d40002ad
RavisAzure <83583293+RavisAzure@users.noreply.github.com>
2021-07-10 20:55:27 +0300

    a

--- src/Components/calendar/Calendar.component.jsx
+++ src/Components/calendar/Calendar.component.jsx
@@ -8,7 +8,7 @@ class CalendarComponent extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            typeOfCalendar: "JAL"
+            typeOfCalendar: "GAR"
         }
     }
     // componentDidMount(){
