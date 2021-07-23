cd9b97370b5a2f38aca2aa72866b5e153cfade66
RavisAzure <83583293+RavisAzure@users.noreply.github.com>
2021-07-23 13:56:04 +0300

    1

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,3 +1,19 @@
+import React from 'react';
+import { Provider } from 'react-redux'
+import { store, persistor } from '../Redux/store';
+//import { PersistGate } from 'redux-persist/integration/react'
+//import reportWebVitals from './reportWebVitals';
+import App from '../../src/sources/App'
+export default function Flights(){
+     return (
+      <Provider store={store}>
+              
+      <App mainRoute='villa'/>
+      {/* <reportWebVitals/> */}
+    </Provider>
+
+);
+}
 // try {
 //     const { data, status } = await GetAdvertsList(page_number);
 //     if (status === 200) {
