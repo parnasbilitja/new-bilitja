2880e7385151e1089d42ff9e4e2ba9095b9bca2d
mehdi <me_mohammadian@yahoo.com>
2021-08-25 12:59:52 +0300

    bank

--- src/pages/api/callbackbank.js
+++ src/pages/api/callbackbank.js
@@ -2,6 +2,7 @@ import { redirect } from "next/dist/next-server/server/api-utils";
 import globals from "../../sources/Global";
 
 export default async function handler(req, res) {
+  
   if (req.method == "POST") {
     const body = req.body;
     //{"message":{"State":"Canceled By User","StateCode":"-1","ResNum":"28284","MID":"10916111","RefNum":"","CID":"","TRACENO":"","RRN":"","SecurePan":""}}
@@ -17,22 +18,22 @@ export default async function handler(req, res) {
 
     switch (body.StateCode) {
       case 1:
-        res.redirect(`https://${req.headers.host}/callbackbank-error?status=1`);
+        res.redirect(`../callbackbank-error?status=1`);
         break;
       case 3:
-        res.redirect(`https://${req.headers.host}/callbackbank-error?status=3`);
+        res.redirect(`../callbackbank-error?status=3`);
         break;
       case 4:
-        res.redirect(`https://${req.headers.host}/callbackbank-error?status=4`);
+        res.redirect(`../callbackbank-error?status=4`);
         break;
       case 5:
-        res.redirect(`https://${req.headers.host}/callbackbank-error?status=5`);
+        res.redirect(`../callbackbank-error?status=5`);
         break;
       case 6:
-        res.redirect(`https://${req.headers.host}/callbackbank-error?status=6`);
+        res.redirect(`../callbackbank-error?status=6`);
         break;
       case 7:
-        res.redirect(`https://${req.headers.host}/callbackbank-error?status=7`);
+        res.redirect(`../callbackbank-error?status=7`);
         break;
       default:
         const response = await fetch(
@@ -42,35 +43,35 @@ export default async function handler(req, res) {
 
         switch (data.status) {
           case "-100":
-            res.redirect(`https://${req.headers.host}/callbackbank-error?-100`);
+            res.redirect(`../callbackbank-error?-100`);
             break;
           case "-101":
             res.redirect(
-              `https://${req.headers.host}/callbackbank-error?status=-101`
+              `../callbackbank-error?status=-101`
             );
             break;
 
           case "-102":
             res.redirect(
-              `https://${req.headers.host}/callbackbank-error?status=-102`
+              `../callbackbank-error?status=-102`
             );
             break;
 
           case "-103":
             res.redirect(
-              `https://${req.headers.host}/callbackbank-error?status=-103`
+              `../callbackbank-error?status=-103`
             );
             break;
 
           case "-104":
             res.redirect(
-              `https://${req.headers.host}/callbackbank-error?status=-104`
+              `../callbackbank-error?status=-104`
             );
             break;
 
           case "-105":
             res.redirect(
-              `https://${req.headers.host}/callbackbank-error?status=-105`
+              `../callbackbank-error?status=-105`
             );
             break;
 
@@ -78,63 +79,11 @@ export default async function handler(req, res) {
             const reqNo = String(data.status).split("|")[0];
             const reqPnr = String(data.status).split("|")[1];
             res.redirect(
-              `https://${req.headers.host}/payment-receipt?reqPnr${reqPnr}&reqNo=${reqNo}`
+              `../payment-receipt?reqPnr${reqPnr}&reqNo=${reqNo}`
             );
             break;
           }
         }
     }
-  } else {
-    //const body2=JSON.parse(req.query);
-
-    //res.send(req.query.ResNum);
-    //  res.status(200).send({message :req.query});
-    // http://localhost:3000/api/callbackbank?StateCode=-1&ResNum=28284&MID=10916111&RefNum=1
-    //   res.send( body.StateCode)
-
-    fetch(
-      `${globals.baseUrl}appCallBackBank/saman/${req.query.StateCode}/${req.query.RefNum}/${req.query.ResNum}/${req.query.MID}`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        // res.send(data);
-        switch (data.status) {
-          case "-100":
-            res.send(data.message);
-            break;
-          case "-101":
-            res.send(data.message);
-            break;
-
-          case "-102":
-            res.send(data.message);
-            break;
-
-          case "-103":
-            res.send("-103");
-            break;
-
-          case "-104":
-            res.send(data.message);
-            break;
-
-          case "-105":
-            res.send(data.message);
-            break;
-
-          default: {
-            const reqNo = String(data.status).split("|")[0];
-            const reqPnr = String(data.status).split("|")[1];
-            //res.send(data.status) ;
-            // if (data.status.search("|") != -1) {
-
-            //   // router.push(`/payment-receipt/?reqPnr=${reqPnr}&reqNo=${reqNo}`);
-            // } else {
-            //   res.send("بانک تراکنش شما را تایید نکرد");
-            // }
-            break;
-          }
-        }
-      });
-  }
+  } 
 }
