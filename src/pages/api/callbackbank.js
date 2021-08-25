d5eb8976ed7d3467cb3589fee59781d2ba6eab36
mehdi <me_mohammadian@yahoo.com>
2021-08-25 17:10:43 +0300

    bank3

--- src/pages/api/callbackbank.js
+++ src/pages/api/callbackbank.js
@@ -1,8 +1,13 @@
+import app from "next/app";
 import { redirect } from "next/dist/next-server/server/api-utils";
 import globals from "../../sources/Global";
+//import { useRouter } from 'next/router'
 
 export default async function handler(req, res) {
-   
+  //const myRouter=useRouter();
+  //myRouter.push(`https://www.google.com`);
+  //console.log('myRouter');
+  
   if (req.method == "POST") {
     const body = req.body;
     //{"message":{"State":"Canceled By User","StateCode":"-1","ResNum":"28284","MID":"10916111","RefNum":"","CID":"","TRACENO":"","RRN":"","SecurePan":""}}
@@ -17,73 +22,75 @@ export default async function handler(req, res) {
     }
 
     switch (body.StateCode) {
-      case 1:
+      case -1:
         res.redirect(`../callbackbank-error?status=1`);
         break;
-      case 3:
+      case -3:
         res.redirect(`../callbackbank-error?status=3`);
         break;
-      case 4:
+      case -4:
         res.redirect(`../callbackbank-error?status=4`);
         break;
-      case 5:
+      case -5:
         res.redirect(`../callbackbank-error?status=5`);
         break;
-      case 6:
+      case -6:
         res.redirect(`../callbackbank-error?status=6`);
         break;
-      case 7:
+      case -7:
         res.redirect(`../callbackbank-error?status=7`);
         break;
       default:
-        const response = await fetch(
-          `${globals.baseUrl}appCallBackBank/saman/${body.StateCode}/${refNum}/${body.ResNum}/${body.MID}`
-        );
-        const data = await response.json();
+        fetch(`${globals.baseUrl}appCallBackBank/saman/${body.StateCode}/${body.RefNum}/${body.ResNum}/${body.message.MID}`)
+        .then(res => res.json()).then(data => { 
+                  switch (data.status) {
+                    case "-100":
+                      res.redirect(`../callbackbank-error?-100`);
+                      break;
+                    case "-101":
+                      res.redirect(
+                        `../callbackbank-error?status=-101`
+                      );
+                      break;
 
-        switch (data.status) {
-          case "-100":
-            res.redirect(`../callbackbank-error?-100`);
-            break;
-          case "-101":
-            res.redirect(
-              `../callbackbank-error?status=-101`
-            );
-            break;
+                    case "-102":
+                      res.redirect(
+                        `../callbackbank-error?status=-102`
+                      );
+                      break;
 
-          case "-102":
-            res.redirect(
-              `../callbackbank-error?status=-102`
-            );
-            break;
+                    case "-103":
+                      res.redirect(
+                        `../callbackbank-error?status=-103`
+                      );
+                      break;
 
-          case "-103":
-            res.redirect(
-              `../callbackbank-error?status=-103`
-            );
-            break;
+                    case "-104":
+                      res.redirect(
+                        `../callbackbank-error?status=-104`
+                      );
+                      break;
 
-          case "-104":
-            res.redirect(
-              `../callbackbank-error?status=-104`
-            );
-            break;
+                    case "-105":
+                      res.redirect(
+                        `../callbackbank-error?status=-105`
+                      );
+                      break;
 
-          case "-105":
-            res.redirect(
-              `../callbackbank-error?status=-105`
-            );
-            break;
-
-          default: {
-            const reqNo = String(data.status).split("|")[0];
-            const reqPnr = String(data.status).split("|")[1];
-            res.redirect(
-              `../payment-receipt?reqPnr${reqPnr}&reqNo=${reqNo}`
-            );
-            break;
-          }
-        }
+                    default: {
+                      const reqNo = String(data.status).split("|")[0];
+                      const reqPnr = String(data.status).split("|")[1];
+                      res.redirect(
+                        `../payment-receipt?reqPnr${reqPnr}&reqNo=${reqNo}`
+                      );
+                      break;
+                    }
+                  }
+        })
     }
   } 
+  else{
+
+    res.redirect("https://www.bilitja.com");
+  }
 }
