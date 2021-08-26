91d3cefec456021155c9448707dbfe080f5cb168
mehdi <me_mohammadian@yahoo.com>
2021-08-26 18:36:36 +0300

    banklast2

--- src/pages/callbackbank.jsx
+++ src/pages/callbackbank.jsx
@@ -1,72 +1,86 @@
 import getRawBody from 'raw-body';
 import { useRouter } from "next/router";
-function Page({ body }) {
+function  Page({ data }) {
   const myRouter = useRouter();
  // myRouter.push(`./callbackbank-error?status=-1`);
-
+ console.log("top"); 
+ console.log(data);
+ console.log("top2"); 
   var refNum = "";
-
-  if (body.RefNum == "") {
+ var error="";
+  if (data.RefNum == "") {
     refNum = "0";
   } else {
-    refNum = body.RefNum;
+    refNum = data.RefNum;
   }
-console.log(body.StateCode);
-  switch (body.StateCode) {
-    case "-1":
-      myRouter.push(`./callbackbank-error?status=-1`);
+console.log(data.StateCode);
+  switch (data.StateCode) {
+    // case "-1":
+    //   myRouter.push(`./callbackbank-error?status=-1`);
       
+    //   break;
+    // case "-3":
+    //   myRouter.push(`./callbackbank-error?status=-3`);
+    //   break;
+    // case "-4":
+    //   myRouter.push(`./callbackbank-error?status=-4`);
+    //   break;
+    // case "-5":
+    //   myRouter.push(`./callbackbank-error?status=-5`);
+    //   break;
+    // case "-6":
+    //   myRouter.push(`./callbackbank-error?status=-6`);
+    //   break;
+    // case "-7":
+    //   myRouter.push(`./callbackbank-error?status=-7`);
+    //   break;
+    case "-1":
+      error = "شما از تراکنش انصراف داده اید.";
       break;
     case "-3":
-      myRouter.push(`./callbackbank-error?status=-3`);
+      error =
+        "پرداخت انجام نشد، در صورت کسر وجه مبلغ با 72 ساعت به حساب شما بازگشت داده می شود.";
       break;
     case "-4":
-      myRouter.push(`./callbackbank-error?status=-4`);
+      error = "متاسفانه مدت زمان انجام تراکنش به اتمام رسیده است.";
       break;
     case "-5":
-      myRouter.push(`./callbackbank-error?status=-5`);
+      error = "اطلاعات ارسال شده نا معتبر است.";
       break;
     case "-6":
-      myRouter.push(`./callbackbank-error?status=-6`);
+      error =
+        "در موارد چند تراکنشی، ازطلاعات موفق از طریق سرویس دیگری ارسال می شود.";
       break;
     case "-7":
-      myRouter.push(`./callbackbank-error?status=-7`);
+      error = "در موارد چند تراکنشی، تمام تراکنش ها پرداخت ناموفق داشته اند.";
       break;
+   
     default:
-      fetch(`${globals.baseUrl}appCallBackBank/saman/${body.StateCode}/${refNum}/${body.ResNum}/${body.MID}`)
+      fetch(`${globals.baseUrl}appCallBackBank/saman/${data.StateCode}/${refNum}/${data.ResNum}/${data.MID}`)
       .then(res => res.json()).then(data => { 
                 switch (data.status) {
                   case "-100":
-                    myRouter.push(`./callbackbank-error?status=-100`);
+                    error = "کد دریافتی از طرف بانک معتبر نیست.";
                     break;
                   case "-101":
-                    myRouter.push(
-                      `./callbackbank-error?status=-101`
-                    );
+                    error = "کد دریافتی از طرف بانک معتبر نیست.";
                     break;
-
+              
                   case "-102":
-                    myRouter.push(
-                      `./callbackbank-error?status=-102`
-                    );
+                    error = "کد دریافتی از طرف بانک معتبر نیست.";
                     break;
-
+              
                   case "-103":
-                    myRouter.push(
-                      `./callbackbank-error?status=-103`
-                    );
+                    error = "بانک تراکنش شما را تایید نکرد";
                     break;
-
+              
                   case "-104":
-                    myRouter.push(
-                      `./callbackbank-error?status=-104`
-                    );
+                    error = "تراکنش انجام نشد. رزرو برای شما ثبت نشده است";
                     break;
-
+              
                   case "-105":
-                    myRouter.push(
-                      `./callbackbank-error?status=-105`
-                    );
+                    error =
+                      "متاسفانه خطا رخ داده است. ممکن است بلیط صادر شده باشد. خواهشمندیم جهت پیگیری رزرو خود حتما با تلفن پشتیبانی تماس حاصل فرمایید";
                     break;
 
                   default: {
@@ -81,14 +95,38 @@ console.log(body.StateCode);
       })
   }
 
-  return( <div>
-    <div>
-    {data}
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        background: "#ccc",
+      }}
+    >
+      <div className="row">
+        <div className="col-12 text-center mb-5">
+          <img src="../../../Images/bilitja.png" className="img-fluid " />
+        </div>
+        <br />
+        <div>
+          <h6 className="font-bold-iransanse text-center alert alert-danger">
+            {" "}
+            خطا: {error}
+          </h6>
+        </div>
+        <div className="mt-5 text-center">
+          <a className="font-bold-iransanse text-center text-danger" href="/">
+            بازگشت به صفحه اصلی
+          </a>
+        </div>
+      </div>
     </div>
-   
-    </div>)
-}
+  );
 
+
+}
 //This gets called on every request
 export async function getServerSideProps({req}) {
   if(req.method=="POST"){
@@ -98,12 +136,15 @@ export async function getServerSideProps({req}) {
         var data={};
         //var body2="State=Canceled+By+User&StateCode=-1&ResNum=28333&MID=10916111&RefNum=&CID=&TRACENO=&RRN=&SecurePan=";
         String(body.toString("utf-8")).split('&').map(x=>( data[x.split('=')[0]] =x.split('=')[1]));
-      //    console.log(data.ResNum);
+        console.log("bottom");   
+        console.log(data);
+        console.log("bottom2");   
       // console.log("end3")
         return { props: {data}  }
   }
   return { props: {}  }
 }
+
 
 export default Page
 
