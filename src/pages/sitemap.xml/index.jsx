6be9f2416cfa1c72108c4cce228553d151201383
mohammadsaleh <salehkarimi595@yahoo.com>
2021-09-25 16:39:45 +0330

    Update

--- src/pages/sitemap.xml/index.jsx
+++ src/pages/sitemap.xml/index.jsx
@@ -1,3 +1,6 @@
+const withXMLTemplate = (content) => {
+  return `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">\n${content}</urlset>`;
+};
 const buildSitemapXml = (fields) => {
   const content = fields
     .map((fieldData) => {
@@ -10,12 +13,8 @@ const buildSitemapXml = (fields) => {
     })
     .join("");
 
-  return this.withXMLTemplate(content);
+  return withXMLTemplate(content);
 };
-
-// const withXMLTemplate = (content) => {
-//   return `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">\n${content}</urlset>`;
-// };
 
 export const getServerSideprops = async ({ res }) => {
   const data = await getAllData();
