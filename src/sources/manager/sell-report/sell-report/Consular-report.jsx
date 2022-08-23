2bdf4df9caea92ae5364268c9ea2724c308d3cac
amirqn <amirqn.dev@gmail.com>
2022-08-23 16:52:08 +0430

    update Comsular-report.jsx

--- src/sources/manager/sell-report/sell-report/Consular-report.jsx
+++ src/sources/manager/sell-report/sell-report/Consular-report.jsx
@@ -5,10 +5,18 @@ import Link from "next/link";
 
 const ConsularReport = () => {
     return (
-        <>
-            <div className="d-flex flex-column align-items-center  w-100">
-                <div className={style["title-page-sales-report"]}>
-                    <h6>گزارش کنسلی</h6>
+        <div>
+            <section>
+                <div class="position-relative">
+                    <h5 className="mt-0">
+                        <span class="font-size-13 font-bold-iransanse mx-2">گـزارشات لغو شده</span>
+                    </h5>
+                    <div class="d-flex align-items-center">
+                        <div class="box-through"></div>
+                        <div class="aside-through"></div>
+                    </div>
+                </div>
+                <div className="d-flex justify-content-end mt-3 align-items-center w-100 mb-4">
                     <div className={style['parent-buttons']}>
                         <Link href="/panel/flight-sell-report">
                             <button>همه </button>
@@ -20,85 +28,86 @@ const ConsularReport = () => {
                             <button>گزارش کنسلی</button>
                         </Link>
                         <Link href="/panel/reserving">
-                            <button>در حال رزرو</button>
+                            <button>در حال رزرو </button>
                         </Link>
                         <Link href="/panel/transaction">
-                            <button>تراکنش ها  </button>
+                            <button>تراکنش ها</button>
                         </Link>
                     </div>
-
                 </div>
-                <div className={style['parent-sales-report']}>
-                    <div className={style['header-parent-sales-report-two']}>
-                        <span className="font-bold-iransanse font-size-12">شماره درخواست </span>
-                        <span className="font-bold-iransanse font-size-12">نام و نام خانوادگی</span>
-                        <span className="font-bold-iransanse font-size-12"> رفرنس بلیطجا</span>
-                        <span className="font-bold-iransanse font-size-12">رفرنس آژانس</span>
-                        <span className="font-bold-iransanse font-size-12">شماره بلیط </span>
-                        <span className="font-bold-iransanse font-size-12">قیمت خرید</span>
-                        <span className="font-bold-iransanse font-size-12">فروش </span>
-                        <span className="font-bold-iransanse font-size-12">شماره همراه رزرو گیرنده </span>
-                        <span className="font-bold-iransanse font-size-12">سود </span>
-                        <span className="font-bold-iransanse font-size-12">بلیط های صادر شده </span>
-                        <span className="font-bold-iransanse font-size-12">ip رزرو    </span>
-                        <span className="font-bold-iransanse font-size-12">بلیط های صادر شده </span>
-                        <span className="font-bold-iransanse font-size-12">  سیستم سرویس  </span>
-                        <span className="font-bold-iransanse font-size-12">   نام آژانس  </span>
-                        <span className="font-bold-iransanse font-size-12">     جریمه کنسلی دریافت </span>
-                        <span className="font-bold-iransanse font-size-12">   پرداختی   </span>
-                        <span className="font-bold-iransanse font-size-12">   قابل استرداد   </span>
-                        <span className="font-bold-iransanse font-size-12">    قابل دریافت تامین کننده  </span>
-                        <span className="font-bold-iransanse font-size-12">   کاربر کنسلی   </span>
-                        <span className="font-bold-iransanse font-size-12">    IP کنسلی   </span>
-                    </div>
-                    <div className={style['body-search-report-two']}>
-                        <span>...</span>
-                        <input type="text"/>
-                        <input type="text"/>
-                        <input type="text"/>
-                        <input type="text"/>
-                        <input type="text"/>
-                        <input type="text"/>
-                        <input type="text"/>
-                        <input type="text"/>
-                        <input type="text"/>
-                        <input type="text"/>
-                        <input type="text"/>
-                        <input type="text"/>
-                        <input type="text"/>
-                        <input type="text"/>
-                        <input type="text"/>
-                        <input type="text"/>
-                        <input type="text"/>
-                        <input type="text"/>
-                        <input type="text"/>
-                    </div>
-                    <div className={style['body-parent-report-two']}>
-                        <span className="id">2 </span>
-                        <span className="font-bold-iransanse font-size-12">نام و نام خانوادگی</span>
-                        <span className="font-bold-iransanse font-size-12"> رفرنس بلیطجا</span>
-                        <span className="font-bold-iransanse font-size-12">رفرنس آژانس</span>
-                        <span className="font-bold-iransanse font-size-12">شماره بلیط </span>
-                        <span className="font-bold-iransanse font-size-12">قیمت خرید</span>
-                        <span className="font-bold-iransanse font-size-12">فروش </span>
-                        <span className="font-bold-iransanse font-size-12">شماره همراه رزرو گیرنده </span>
-                        <span className="font-bold-iransanse font-size-12">سود </span>
-                        <span className="font-bold-iransanse font-size-12">بلیط های صادر شده </span>
-                        <span className="font-bold-iransanse font-size-12">ip رزرو    </span>
-                        <span className="font-bold-iransanse font-size-12">بلیط های صادر شده </span>
-                        <span className="font-bold-iransanse font-size-12">  سیستم سرویس  </span>
-                        <span className="font-bold-iransanse font-size-12">   نام آژانس  </span>
-                        <span className="font-bold-iransanse font-size-12">     جریمه کنسلی دریافت </span>
-                        <span className="font-bold-iransanse font-size-12">   پرداختی   </span>
-                        <span className="font-bold-iransanse font-size-12">   قابل استرداد   </span>
-                        <span className="font-bold-iransanse font-size-12">    قابل دریافت تامین کننده  </span>
-                        <span className="font-bold-iransanse font-size-12">   کاربر کنسلی   </span>
-                        <span className="font-bold-iransanse font-size-12">    IP کنسلی   </span>
-                    </div>
+                <div className="d-flex flex-column align-items-center w-100">
+                    <div className={style['parent-sales-report']}>
+                        <div className={style['header-parent-sales-report-two']}>
+                            <span className="font-bold-iransanse font-size-12">شماره درخواست </span>
+                            <span className="font-bold-iransanse font-size-12">نام و نام خانوادگی</span>
+                            <span className="font-bold-iransanse font-size-12"> رفرنس بلیطجا</span>
+                            <span className="font-bold-iransanse font-size-12">رفرنس آژانس</span>
+                            <span className="font-bold-iransanse font-size-12">شماره بلیط </span>
+                            <span className="font-bold-iransanse font-size-12">قیمت خرید</span>
+                            <span className="font-bold-iransanse font-size-12">فروش </span>
+                            <span className="font-bold-iransanse font-size-12">شماره همراه رزرو گیرنده </span>
+                            <span className="font-bold-iransanse font-size-12">سود </span>
+                            <span className="font-bold-iransanse font-size-12">بلیط های صادر شده </span>
+                            <span className="font-bold-iransanse font-size-12">ip رزرو    </span>
+                            <span className="font-bold-iransanse font-size-12">بلیط های صادر شده </span>
+                            <span className="font-bold-iransanse font-size-12">  سیستم سرویس  </span>
+                            <span className="font-bold-iransanse font-size-12">   نام آژانس  </span>
+                            <span className="font-bold-iransanse font-size-12">     جریمه کنسلی دریافت </span>
+                            <span className="font-bold-iransanse font-size-12">   پرداختی   </span>
+                            <span className="font-bold-iransanse font-size-12">   قابل استرداد   </span>
+                            <span className="font-bold-iransanse font-size-12">    قابل دریافت تامین کننده  </span>
+                            <span className="font-bold-iransanse font-size-12">   کاربر کنسلی   </span>
+                            <span className="font-bold-iransanse font-size-12">    IP کنسلی   </span>
+                        </div>
+                        <div className={style['body-search-report-two']}>
+                            <span>...</span>
+                            <input type="text" />
+                            <input type="text" />
+                            <input type="text" />
+                            <input type="text" />
+                            <input type="text" />
+                            <input type="text" />
+                            <input type="text" />
+                            <input type="text" />
+                            <input type="text" />
+                            <input type="text" />
+                            <input type="text" />
+                            <input type="text" />
+                            <input type="text" />
+                            <input type="text" />
+                            <input type="text" />
+                            <input type="text" />
+                            <input type="text" />
+                            <input type="text" />
+                            <input type="text" />
+                        </div>
+                        <div className={style['body-parent-report-two']}>
+                            <span className="id">2 </span>
+                            <span className="font-bold-iransanse font-size-12">نام و نام خانوادگی</span>
+                            <span className="font-bold-iransanse font-size-12"> رفرنس بلیطجا</span>
+                            <span className="font-bold-iransanse font-size-12">رفرنس آژانس</span>
+                            <span className="font-bold-iransanse font-size-12">شماره بلیط </span>
+                            <span className="font-bold-iransanse font-size-12">قیمت خرید</span>
+                            <span className="font-bold-iransanse font-size-12">فروش </span>
+                            <span className="font-bold-iransanse font-size-12">شماره همراه رزرو گیرنده </span>
+                            <span className="font-bold-iransanse font-size-12">سود </span>
+                            <span className="font-bold-iransanse font-size-12">بلیط های صادر شده </span>
+                            <span className="font-bold-iransanse font-size-12">ip رزرو    </span>
+                            <span className="font-bold-iransanse font-size-12">بلیط های صادر شده </span>
+                            <span className="font-bold-iransanse font-size-12">  سیستم سرویس  </span>
+                            <span className="font-bold-iransanse font-size-12">   نام آژانس  </span>
+                            <span className="font-bold-iransanse font-size-12">     جریمه کنسلی دریافت </span>
+                            <span className="font-bold-iransanse font-size-12">   پرداختی   </span>
+                            <span className="font-bold-iransanse font-size-12">   قابل استرداد   </span>
+                            <span className="font-bold-iransanse font-size-12">    قابل دریافت تامین کننده  </span>
+                            <span className="font-bold-iransanse font-size-12">   کاربر کنسلی   </span>
+                            <span className="font-bold-iransanse font-size-12">    IP کنسلی   </span>
+                        </div>
 
+                    </div>
                 </div>
-            </div>
-        </>
+            </section>
+        </div>
     )
 }
 export default ConsularReport;
