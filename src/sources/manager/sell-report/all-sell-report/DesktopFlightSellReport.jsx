d48973afaa462056e4d703678a156f5539a686cd
amirqn <amirqn.dev@gmail.com>
2022-08-23 16:05:23 +0430

    update DesktopFlightSellReport.jsx

--- src/sources/manager/sell-report/all-sell-report/DesktopFlightSellReport.jsx
+++ src/sources/manager/sell-report/all-sell-report/DesktopFlightSellReport.jsx
@@ -82,7 +82,7 @@ const EnhancedTableHead = (props) => {
               key={headCell.id}
               align="center"
               padding="none"
-              // className="pt-1 pb-1"
+            // className="pt-1 pb-1"
             >
               <h6 className="text-dark font-size-11">{headCell.label}</h6>
             </TableCell>
@@ -214,190 +214,200 @@ const AllFlightSellReport = () => {
   const isSelected = (name) => selected.indexOf(name) !== -1;
 
   return (
-    <div className="container pt-2 pb-2 pe-2 ps-2">
-      <h2 className={style['title-page']}>ریز گزارشات فروش پرواز</h2>
-      <TopFilter />
-      <Box>
-        <Paper>
-          <TableContainer>
-            <Table
-              sx={{ minWidth: 750 }}
-              aria-labelledby="tableTitle"
-              size="small"
+    <section>
+      <div>
+        <div class="position-relative">
+          <h5 className="mt-0">
+            <span class="font-size-13 font-bold-iransanse mx-2">ریز گــزارشات فـروش پـرواز</span>
+          </h5>
+          <div class="d-flex align-items-center">
+            <div class="box-through"></div>
+            <div class="aside-through"></div>
+          </div>
+        </div>
+        <TopFilter />
+        <Box>
+          <Paper>
+            <TableContainer>
+              <Table
+                sx={{ minWidth: 750 }}
+                aria-labelledby="tableTitle"
+                size="small"
+              >
+                <EnhancedTableHead
+                  numSelected={selected.length}
+                  order={order}
+                  orderBy={orderBy}
+                  onRequestSort={handleRequestSort}
+                  rowCount={
+                    flights.filter_details_sell_report == null
+                      ? []
+                      : flights.filter_details_sell_report.length
+                  }
+                />
+                <TableBody>
+                  {stableSort(
+                    flights.filter_details_sell_report == null
+                      ? []
+                      : flights.filter_details_sell_report,
+                    getComparator(order, orderBy)
+                  )
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map((row, index) => {
+                      const isItemSelected = isSelected(row.name);
+
+                      return (
+                        <TableRow
+                          tabIndex={-1}
+                          key={row.name}
+                          selected={isItemSelected}
+                          className="cursor-pointer hover-tabel-row"
+                        >
+                          <TableCell align="center">
+                            <h6 className="font-size-11">{i++}</h6>
+                          </TableCell>
+                          <TableCell align="center">
+                            <h6 className="font-size-11">{row.changeStat}</h6>
+                          </TableCell>
+                          <TableCell align="center">
+                            <h6 className="font-size-11">{row.customerName}</h6>
+                          </TableCell>
+                          <TableCell align="center">
+                            <h6 className="font-size-11">{row.reqNo}</h6>
+                          </TableCell>
+                          <TableCell align="center">
+                            <h6 className="font-size-11">{row.reqPnr}</h6>
+                          </TableCell>
+                          <TableCell align="center">
+                            <h6 className="font-size-11">{row.dateTimeSabt}</h6>
+                          </TableCell>
+                          <TableCell align="center">
+                            <h6 className="font-size-11">{row.changeStat}</h6>
+                          </TableCell>
+                          <TableCell align="center">
+                            <h6 className="font-size-11">{row.reqNoRow}</h6>
+                          </TableCell>
+                          <TableCell align="center">
+                            <h6 className="font-size-11">{row.nameFamilyEn}</h6>
+                          </TableCell>
+                          <TableCell align="center">
+                            <h6 className="font-size-11">{row.meliCode}</h6>
+                          </TableCell>
+                          <TableCell align="center">
+                            <h6 className="font-size-11">{row.ticketName}</h6>
+                          </TableCell>
+                          <TableCell align="center">
+                            <h6 className="font-size-11">{row.ticketNo}</h6>
+                          </TableCell>
+                          <TableCell align="center">
+                            <h6 className="font-size-11">{row.sex}</h6>
+                          </TableCell>
+                          <TableCell align="center">
+                            <h6 className="font-size-11">{row.userFr}</h6>
+                          </TableCell>
+                          <TableCell align="center">
+                            <h6 className="font-size-11">
+                              {moneyFormat(row.ticketPrice)}
+                            </h6>
+                          </TableCell>
+                          <TableCell align="center">
+                            <h6 className="font-size-11">
+                              {moneyFormat(row.ticketPriceFare)}
+                            </h6>
+                          </TableCell>
+                          <TableCell align="center">
+                            <h6 className="font-size-11">
+                              {moneyFormat(row.ticketPriceKh)}
+                            </h6>
+                          </TableCell>
+                          <TableCell align="center">
+                            <h6 className="font-size-11">
+                              {moneyFormat(row.stock)}
+                            </h6>
+                          </TableCell>
+                          <TableCell align="center">
+                            <h6 className="font-size-11">{row.route}</h6>
+                          </TableCell>
+                          <TableCell align="center">
+                            <h6 className="font-size-11">{row.flightNo}</h6>
+                          </TableCell>
+                          <TableCell align="center">
+                            <h6 className="font-size-11">{row.flightDate}</h6>
+                          </TableCell>
+                          <TableCell align="center">
+                            <h6 className="font-size-11">{row.airline}</h6>
+                          </TableCell>
+                          <TableCell align="center">
+                            <h6 className="font-size-11">{row.className}</h6>
+                          </TableCell>
+                          <TableCell align="center">
+                            <h6 className="font-size-11">{row.kndSysName}</h6>
+                          </TableCell>
+                          <TableCell align="center">
+                            <h6 className="font-size-11">{row.serviceName}</h6>
+                          </TableCell>
+                          <TableCell align="center">
+                            <h6 className="font-size-11">{row.servicePnr}</h6>
+                          </TableCell>
+                          <TableCell align="center">
+                            <h6 className="font-size-11">{row.pathKind}</h6>
+                          </TableCell>
+                          <TableCell align="center">
+                            <h6 className="font-size-11">{row.finePrice}</h6>
+                          </TableCell>
+                          <TableCell align="center">
+                            <h6 className="font-size-11">{row.changeTime}</h6>
+                          </TableCell>
+                          <TableCell align="center">
+                            <h6 className="font-size-11">{row.changeUser}</h6>
+                          </TableCell>
+                          <TableCell align="center">
+                            <h6 className="font-size-11">{row.changeIp}</h6>
+                          </TableCell>
+                          <TableCell align="center">
+                            <h6 className="font-size-11">{row.changeStatName}</h6>
+                          </TableCell>
+                          <TableCell align="center">
+                            <h6 className="font-size-11">{row.finePriceName}</h6>
+                          </TableCell>
+                          <TableCell align="center">
+                            <h6 className="font-size-11">{row.changeTimeName}</h6>
+                          </TableCell>
+                        </TableRow>
+                      );
+                    })}
+                  <EndRow
+                    ticketPrice={prices.foroshAll}
+                    ticketPriceFare={prices.foroshFareAll}
+                    ticketPriceKh={prices.kharidAll}
+                    stock={prices.soodAll}
+                  />
+                </TableBody>
+              </Table>
+            </TableContainer>
+            <div
+              className={`d-flex justify-content-start pe-1 ps-1 font-size-16${style["title"]}`}
+              dir="ltr"
             >
-              <EnhancedTableHead
-                numSelected={selected.length}
-                order={order}
-                orderBy={orderBy}
-                onRequestSort={handleRequestSort}
-                rowCount={
+              <TablePagination
+                rowsPerPageOptions={[5, 10, 25, 50, 75, 100]}
+                component="div"
+                count={
                   flights.filter_details_sell_report == null
                     ? []
                     : flights.filter_details_sell_report.length
                 }
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+                labelRowsPerPage="آیتم نمایشی هر صفحه:"
               />
-              <TableBody>
-                {stableSort(
-                  flights.filter_details_sell_report == null
-                    ? []
-                    : flights.filter_details_sell_report,
-                  getComparator(order, orderBy)
-                )
-                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                  .map((row, index) => {
-                    const isItemSelected = isSelected(row.name);
-
-                    return (
-                      <TableRow
-                        tabIndex={-1}
-                        key={row.name}
-                        selected={isItemSelected}
-                        className="cursor-pointer hover-tabel-row"
-                      >
-                        <TableCell align="center">
-                          <h6 className="font-size-11">{i++}</h6>
-                        </TableCell>
-                        <TableCell align="center">
-                          <h6 className="font-size-11">{row.changeStat}</h6>
-                        </TableCell>
-                        <TableCell align="center">
-                          <h6 className="font-size-11">{row.customerName}</h6>
-                        </TableCell>
-                        <TableCell align="center">
-                          <h6 className="font-size-11">{row.reqNo}</h6>
-                        </TableCell>
-                        <TableCell align="center">
-                          <h6 className="font-size-11">{row.reqPnr}</h6>
-                        </TableCell>
-                        <TableCell align="center">
-                          <h6 className="font-size-11">{row.dateTimeSabt}</h6>
-                        </TableCell>
-                        <TableCell align="center">
-                          <h6 className="font-size-11">{row.changeStat}</h6>
-                        </TableCell>
-                        <TableCell align="center">
-                          <h6 className="font-size-11">{row.reqNoRow}</h6>
-                        </TableCell>
-                        <TableCell align="center">
-                          <h6 className="font-size-11">{row.nameFamilyEn}</h6>
-                        </TableCell>
-                        <TableCell align="center">
-                          <h6 className="font-size-11">{row.meliCode}</h6>
-                        </TableCell>
-                        <TableCell align="center">
-                          <h6 className="font-size-11">{row.ticketName}</h6>
-                        </TableCell>
-                        <TableCell align="center">
-                          <h6 className="font-size-11">{row.ticketNo}</h6>
-                        </TableCell>
-                        <TableCell align="center">
-                          <h6 className="font-size-11">{row.sex}</h6>
-                        </TableCell>
-                        <TableCell align="center">
-                          <h6 className="font-size-11">{row.userFr}</h6>
-                        </TableCell>
-                        <TableCell align="center">
-                          <h6 className="font-size-11">
-                            {moneyFormat(row.ticketPrice)}
-                          </h6>
-                        </TableCell>
-                        <TableCell align="center">
-                          <h6 className="font-size-11">
-                            {moneyFormat(row.ticketPriceFare)}
-                          </h6>
-                        </TableCell>
-                        <TableCell align="center">
-                          <h6 className="font-size-11">
-                            {moneyFormat(row.ticketPriceKh)}
-                          </h6>
-                        </TableCell>
-                        <TableCell align="center">
-                          <h6 className="font-size-11">
-                            {moneyFormat(row.stock)}
-                          </h6>
-                        </TableCell>
-                        <TableCell align="center">
-                          <h6 className="font-size-11">{row.route}</h6>
-                        </TableCell>
-                        <TableCell align="center">
-                          <h6 className="font-size-11">{row.flightNo}</h6>
-                        </TableCell>
-                        <TableCell align="center">
-                          <h6 className="font-size-11">{row.flightDate}</h6>
-                        </TableCell>
-                        <TableCell align="center">
-                          <h6 className="font-size-11">{row.airline}</h6>
-                        </TableCell>
-                        <TableCell align="center">
-                          <h6 className="font-size-11">{row.className}</h6>
-                        </TableCell>
-                        <TableCell align="center">
-                          <h6 className="font-size-11">{row.kndSysName}</h6>
-                        </TableCell>
-                        <TableCell align="center">
-                          <h6 className="font-size-11">{row.serviceName}</h6>
-                        </TableCell>
-                        <TableCell align="center">
-                          <h6 className="font-size-11">{row.servicePnr}</h6>
-                        </TableCell>
-                        <TableCell align="center">
-                          <h6 className="font-size-11">{row.pathKind}</h6>
-                        </TableCell>
-                        <TableCell align="center">
-                          <h6 className="font-size-11">{row.finePrice}</h6>
-                        </TableCell>
-                        <TableCell align="center">
-                          <h6 className="font-size-11">{row.changeTime}</h6>
-                        </TableCell>
-                        <TableCell align="center">
-                          <h6 className="font-size-11">{row.changeUser}</h6>
-                        </TableCell>
-                        <TableCell align="center">
-                          <h6 className="font-size-11">{row.changeIp}</h6>
-                        </TableCell>
-                        <TableCell align="center">
-                          <h6 className="font-size-11">{row.changeStatName}</h6>
-                        </TableCell>
-                        <TableCell align="center">
-                          <h6 className="font-size-11">{row.finePriceName}</h6>
-                        </TableCell>
-                        <TableCell align="center">
-                          <h6 className="font-size-11">{row.changeTimeName}</h6>
-                        </TableCell>
-                      </TableRow>
-                    );
-                  })}
-                <EndRow
-                  ticketPrice={prices.foroshAll}
-                  ticketPriceFare={prices.foroshFareAll}
-                  ticketPriceKh={prices.kharidAll}
-                  stock={prices.soodAll}
-                />
-              </TableBody>
-            </Table>
-          </TableContainer>
-          <div
-            className={`d-flex justify-content-start pe-1 ps-1 font-size-16${style["title"]}`}
-            dir="ltr"
-          >
-            <TablePagination
-              rowsPerPageOptions={[5, 10, 25, 50, 75, 100]}
-              component="div"
-              count={
-                flights.filter_details_sell_report == null
-                  ? []
-                  : flights.filter_details_sell_report.length
-              }
-              rowsPerPage={rowsPerPage}
-              page={page}
-              onPageChange={handleChangePage}
-              onRowsPerPageChange={handleChangeRowsPerPage}
-              labelRowsPerPage="آیتم نمایشی هر صفحه:"
-            />
-          </div>
-        </Paper>
-      </Box>
-    </div>
+            </div>
+          </Paper>
+        </Box>
+      </div>
+    </section>
   );
 };
 
