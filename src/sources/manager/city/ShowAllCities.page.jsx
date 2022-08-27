c6f1bb62185811557ec549bd39be596b68782678
amirqn <amirqn.dev@gmail.com>
2022-08-27 17:27:26 +0430

    update ShowAllCities.page.jsx

--- src/sources/manager/city/ShowAllCities.page.jsx
+++ src/sources/manager/city/ShowAllCities.page.jsx
@@ -30,75 +30,77 @@ const ShowallCities = (props) => {
   const myRouter = useRouter();
 
   return (
-    <div className={styles["panel-main-content"]}>
-      <div className="panel-header">
-        <div class="position-relative">
-          <h6 className="mt-0 font-bold-iransanse">
-            لیست شهــرها
-          </h6>
-          <div class="d-flex align-items-center position-absolute" style={{ top: '27px' }}>
-            <div class="box-through"></div>
-            <div class="aside-through"></div>
+    <section>
+      <div>
+        <div className="panel-header">
+          <div class="position-relative">
+            <h6 className="mt-0 font-bold-iransanse">
+              لیست شهــرها
+            </h6>
+            <div class="d-flex align-items-center position-absolute" style={{ top: '27px' }}>
+              <div class="box-through"></div>
+              <div class="aside-through"></div>
+            </div>
           </div>
-        </div>
-        <div className="text-left ltr">
-          <ManagerTopActionBox
-            handleClick={() => {
-              myRouter.push("/panel/city/add");
-            }}
-            titlebase="شهــر"
-          />
-        </div>
-      </div>
-      <div className="margin-top-10px">
-        {cities.map((city) => (
-          <div className={styles["management-one-row"]}>
-            <span>{city.ProvinceName}</span>
-            <span
-              onClick={() => {
-                myRouter.push(`${this.props.router.asPath}/${city.CityId}`);
+          <div className="text-left ltr">
+            <ManagerTopActionBox
+              handleClick={() => {
+                myRouter.push("/panel/city/add");
               }}
-            >
-              {city.CityName}
-            </span>
-
-            <FontAwesomeIcon
-              icon={faTrash}
-              onClick={() => {
-                const answer = window.confirm("آیا از حذف اطمینان دارید");
-                if (answer) {
-                  fetch(`${globals.baseUrl}bj/city/delete`, {
-                    method: "POST",
-                    headers: { "Content-Type": "application/json" },
-                    body: JSON.stringify({
-                      CityId: city.CityId,
-                    }),
-                  })
-                    .then((res) => res.json())
-                    .then((data) => {
-                      if (data.status == "OK") {
-                        props
-                          .messageBoxModify({
-                            state: true,
-                            message: "عملیات موفقیت آمیز بود",
-                          })
-                          .then(() => {
-                            getData();
-                          });
-                      } else {
-                        props.messageBoxModify({
-                          state: true,
-                          message: data.message,
-                        });
-                      }
-                    });
-                }
-              }}
+              titlebase="شهــر"
             />
           </div>
-        ))}
+        </div>
+        <div className="margin-top-10px">
+          {cities.map((city) => (
+            <div className={styles["management-one-row"]}>
+              <span>{city.ProvinceName}</span>
+              <span
+                onClick={() => {
+                  myRouter.push(`${this.props.router.asPath}/${city.CityId}`);
+                }}
+              >
+                {city.CityName}
+              </span>
+
+              <FontAwesomeIcon
+                icon={faTrash}
+                onClick={() => {
+                  const answer = window.confirm("آیا از حذف اطمینان دارید");
+                  if (answer) {
+                    fetch(`${globals.baseUrl}bj/city/delete`, {
+                      method: "POST",
+                      headers: { "Content-Type": "application/json" },
+                      body: JSON.stringify({
+                        CityId: city.CityId,
+                      }),
+                    })
+                      .then((res) => res.json())
+                      .then((data) => {
+                        if (data.status == "OK") {
+                          props
+                            .messageBoxModify({
+                              state: true,
+                              message: "عملیات موفقیت آمیز بود",
+                            })
+                            .then(() => {
+                              getData();
+                            });
+                        } else {
+                          props.messageBoxModify({
+                            state: true,
+                            message: data.message,
+                          });
+                        }
+                      });
+                  }
+                }}
+              />
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 const mapDispatchesToProps = (dispatch) => ({
