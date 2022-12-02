d37b8db917ec46c723793f6c42b1492f475d3aa0
amirqn <amirqn.dev@gmail.com>
2022-12-02 19:14:09 +0330

    update file Row.jsx

--- src/sources/dashboard/orders/Row.jsx
+++ src/sources/dashboard/orders/Row.jsx
@@ -1,35 +1,35 @@
 import React from 'react';
 
-const Row = ({props}) => {
-    return (
-        <div className="d-detail d-flex align-items-center" >
-                {/* <div className="detail flex-5 m-flex-10"></div> */}
-                <div className="detail flex-7 m-flex-15">
-                  <span className="font-size-14">100056</span>
-                </div>
-                <div className="detail flex-25 m-flex-50">
-                  <span className="font-size-14">{props.nameFamily}</span>
-                </div>
-                <div className="detail flex-14 m-flex-20">
-                  <span className="font-size-14">{props.mobileNo}</span>
-                </div>
-                <div className="detail flex-14 m-flex-20">
-                  <span className="font-size-14">{props.dateTimeSabt}</span>
-                </div>
-                <div className="detail flex-14 m-flex-20">
-                  <span className="font-size-14">{props.amount} تومان</span>
-                </div>
-                <div className="detail flex-14 m-flex-20">
-                  <span className="font-size-14">{props.stat}</span>
-                </div>
-                <div className="detail flex-14 m-flex-20">
-                  <span className="font-size-14">{props.reqPnr}</span>
-                </div>
-                <div className="detail flex-14 m-flex-20">
-                  <span className="font-size-14">{props.stat}</span>
-                </div>
-              </div>
-    );
+const Row = ({ props }) => {
+  return (
+    <div className="d-detail d-flex align-items-center" >
+      {/* <div className="detail flex-5 m-flex-10"></div> */}
+      <div className="detail flex-7 m-flex-20">
+        <span className="font-size-14">100056</span>
+      </div>
+      <div className="detail flex-25 m-flex-100">
+        <span className="font-size-14">{props.nameFamily}</span>
+      </div>
+      <div className="detail flex-14 m-flex-80">
+        <span className="font-size-14">{props.mobileNo}</span>
+      </div>
+      <div className="detail flex-14 m-flex-60">
+        <span className="font-size-14">{props.dateTimeSabt}</span>
+      </div>
+      <div className="detail flex-14 m-flex-60">
+        <span className="font-size-14">{props.amount} تومان</span>
+      </div>
+      <div className="detail flex-14 m-flex-60">
+        <span className="font-size-14">{props.stat}</span>
+      </div>
+      <div className="detail flex-14 m-flex-60">
+        <span className="font-size-14">{props.reqPnr}</span>
+      </div>
+      <div className="detail flex-14 m-flex-60">
+        <span className="font-size-14">{props.stat}</span>
+      </div>
+    </div>
+  );
 };
 
 export default Row;
