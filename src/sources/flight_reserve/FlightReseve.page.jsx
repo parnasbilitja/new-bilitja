52b3a8e2febe845f27858c9950bd469266a20df1
hosseinafp <hosseinafp7@gmail.com>
2022-12-31 12:57:07 +0330

    .

--- src/sources/flight_reserve/FlightReseve.page.jsx
+++ src/sources/flight_reserve/FlightReseve.page.jsx
@@ -30,10 +30,7 @@ import PopUp from "../component/PopUp.component";
 
 const FlightReserve = (props) =>{
     useEffect(() => {
-        // window.scrollTo(0, 0,);
-        var flightdate = location.hash.substring(1);
-        console.log(flightdate);
-        location.reload();
+        window.scrollTo(0, 0,);
       }, []);
     // console.log(props);
     const [err,setErr] = useState({
