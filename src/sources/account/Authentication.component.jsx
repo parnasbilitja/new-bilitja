f7e3cc97d4f0149eedd7c9cd9f99a88aad969db9
amirqn <amirqn.dev@gmail.com>
2023-01-03 13:39:01 +0330

    update file Authentication.component.jsx

--- src/sources/account/Authentication.component.jsx
+++ src/sources/account/Authentication.component.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { CloseOutlined } from "@ant-design/icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes, faLock } from "@fortawesome/free-solid-svg-icons";
 import PrimaryTextInput from "../component/PrimaryTextInput.component";
@@ -12,39 +13,40 @@ import { addAccountProperties } from "../../Redux/Account/account.action";
 import globals from "../Global";
 import { Loader } from "./../../Utils/Loader";
 const Authentication = (props) => {
-    const [state,setState] = useState({
-      token: "",
-      forseUpdate: false,
-      btn_disabled: false,
-      loading: false,
-    });
-  
+  const [state, setState] = useState({
+    token: "",
+    forseUpdate: false,
+    btn_disabled: false,
+    loading: false,
+  });
+
   const handleChange = (event) => {
     const { name, value } = event.target;
-    setState({...state,
+    setState({
+      ...state,
       [name]: value,
     });
   };
 
   const loginWithToken = () => {
-    setState({...state, btn_disabled: true, loading: true });
+    setState({ ...state, btn_disabled: true, loading: true });
     fetch(`${globals.baseUrlNew}auth/checkUser`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         mobile: props.mobile,
         token: state.token,
-        hostname : "bilitja.com",
-        customerId : "1a157116-a01a-4027-ab10-74098ac63815",
-        agencyName : "بلیطجا",
-        telNumber : "02157874",
+        hostname: "bilitja.com",
+        customerId: "1a157116-a01a-4027-ab10-74098ac63815",
+        agencyName: "بلیطجا",
+        telNumber: "02157874",
 
       }),
     })
       .then((res) => res.json())
       .then((data) => {
         if (data.status == "0") {
-          setState({...state, btn_disabled: false, loading: false });
+          setState({ ...state, btn_disabled: false, loading: false });
           localStorage.setItem("mobile", data.mobile);
           localStorage.setItem("token", data.token);
           setTimeout(() => {
@@ -60,67 +62,67 @@ const Authentication = (props) => {
           });
           props.messageBoxModify({
             state: true,
-            color:true,
+            color: true,
             message: "ورود شما موفقیت آمیز بود.",
           });
         } else {
-          setState({...state, btn_disabled: false, loading: false });
+          setState({ ...state, btn_disabled: false, loading: false });
           props.messageBoxModify({
-            color:false,
+            color: false,
             state: true,
             message: data.message,
           });
         }
       });
   };
-    return (
-      <div className="popup-content-container">
-        <div className="popup-heading">
-          <span>ورود</span>
-          <span
-            className="pull-left exit-form"
-            onClick={() => {
-              props.accountBoxModify({
-                state: false,
-              });
-            }}
-          >
-            <FontAwesomeIcon icon={faTimes} />
-          </span>
+  return (
+    <div className="popup-content-container">
+      <div className="popup-heading d-flex align-items-center justify-content-center">
+        <span>ورود</span>
+        <span
+          className="exit-form position-absolute"
+          onClick={() => {
+            props.accountBoxModify({
+              state: false,
+            });
+          }} style={{ left: 10 }}
+        >
+          <CloseOutlined style={{ color: "red" }} />
+        </span>
+      </div>
+      <div className="row">
+        <div className="col-1 padding-horizental-3px">
+          <FontAwesomeIcon icon={faLock} className="margin-top-20px" />
         </div>
-        <div className="row">
-          <div className="col-1 padding-horizental-3px">
-            <FontAwesomeIcon icon={faLock} className="margin-top-20px" />
-          </div>
-          <div className="col-11 padding-horizental-3px">
-            <div className={` form-input-border `}>
-              <PrimaryTextInput
-                placeholder="کد ارسال شده"
-                name="token"
-                onChange={handleChange}
-              />
-            </div>
+        <div className="col-11 padding-horizental-3px">
+          <div className={` form-input-border `}>
+            <PrimaryTextInput
+              placeholder="کد ارسال شده"
+              name="token"
+              onChange={handleChange}
+            />
           </div>
         </div>
+      </div>
 
-        <div className="row">
-          <div className="form-input-border without-focus col-12">
-            <button
-              onClick={(e) => {
-                loginWithToken();
-              }}
-              className={
-                props.disabled === false
-                  ? "btn btn-info py-3 mb-3 col-12 btn-block"
-                  : styles["primary-button"]
-              }
-              disabled={state.btn_disabled}
-            >
-              {state.loading === false ? "  ورود" : <Loader />}
-            </button>
-          </div>
+      <div className="row">
+        <div className="form-input-border without-focus col-12">
+          <button
+            onClick={(e) => {
+              loginWithToken();
+            }}
+            className={
+              props.disabled === false
+                ? "btn btn-info py-3 mb-3 col-12 btn-block"
+                : styles["primary-button"]
+            }
+            disabled={state.btn_disabled}
+          >
+            {state.loading === false ? "  ورود" : <Loader />}
+          </button>
         </div>
-        {/* <div className="row">
+      </div>
+      {/* <div className="row">
                     <div className="col-lg-6 col-md-6 col-sm-6 col-12 no-padding-horizental">
                         <br />
                         <p className="text-center no-margin font-size-13" onClick={() => {
@@ -140,8 +142,8 @@ const Authentication = (props) => {
                         }}>ثبت نام</p>
                     </div>
                 </div> */}
-      </div>
-    );
+    </div>
+  );
 }
 const mapDispatchesToProps = (dispatch) => ({
   accountBoxModify: (value) => dispatch(accountBoxModify(value)),
