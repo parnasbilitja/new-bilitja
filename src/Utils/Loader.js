3b49fef6ca8274467ac264c95142da372e0e4a17
hosseinafp <hosseinafp7@gmail.com>
2023-04-15 16:24:36 +0430

    alt for loader

--- src/Utils/Loader.js
+++ src/Utils/Loader.js
@@ -5,7 +5,7 @@ export const Loader = () => {
     <>
       <div className="loader-data d-flex flex-column align-items-center justify-content-center" style={{ width: '100%', height: '35vh' }}>
         <div className="image">
-          <img src="../../Images/Loading_Circle2.gif" width={'150px'} height={'150px'} alt="loader" />
+          <img src="../../Images/Loading_Circle2.gif" width={'150px'} height={'150px'} alt="please wait for data" />
         </div>
         <div className="text mt-5">
           <span className="font-yekan font-bold font-size-24">لــطفا صــبر کـنید</span>
