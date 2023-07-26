b6383ee6883f9058d77ce8d03868d47db1d33ddd
parnasbilitja <136015423+parnasbilitja@users.noreply.github.com>
2023-07-26 17:55:59 +0330

    staycount checkin checkout

--- src/pages/tours/reserve/[...reservedHotel].js
+++ src/pages/tours/reserve/[...reservedHotel].js
@@ -4,12 +4,12 @@ import Reservation from "../../../Components/NewTours/Reservation";
 import NavHandler from "../../../Components/share/NavHandler";
 import axios from "axios";
 import { jalaliToMiladiConvertor } from "../../../Utils/newTour";
+import moment from "moment-jalaali";
 const ReservedHotel = () => {
   const router = useRouter();
   const [hotelDet, setHoteldet] = useState([]);
   const [stayCount, setStayCount] = useState();
   useEffect(() => {
-    console.log("dasda", jalaliToMiladiConvertor(router.query.checkin));
     if (router.query.rooms && router.query.reservedHotel) {
       const rooms = JSON.parse(router.query.rooms);
       const checkin = jalaliToMiladiConvertor(router.query.checkin);
@@ -17,7 +17,8 @@ const ReservedHotel = () => {
       const flight_id = +router.query.reservedHotel[1];
       const hotel_id = +router.query.reservedHotel[0];
       // const stayCount = router.query.stayCount;
-      setStayCount(stayCount);
+      // console.log("dasda", moment(checkout).diff(checkin, "days"));
+      setStayCount(moment(checkout).diff(checkin, "days"));
       axios
         .post(
           "https://hotelobilit-api.iran.liara.run/api/v1/reserves/checking",
