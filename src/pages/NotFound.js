c774884d15008831563d9c4d4c9b7fcab61f30dc
Pouya-A <58390637+Pouya-A@users.noreply.github.com>
2024-08-19 11:24:36 +0330

    some changes

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -2,19 +2,21 @@ import React from 'react';
 import Link from "next/link";
 
 
-const NotFound = () => {
+const NotFound = (props) => {
     return (
         <>
             <div className="not-found-container">
                 <img
                     width=""
                     height=""
-                    alt="بلیطجا-لوگو"
-                    src="../../../Images/hamnavaz-logo.webp"
+                    alt="همنواز-لوگو"
+                    src="../../../Images/bilitja-logo.webp"
                 />
 
                 <h5 className="font-bold-iransanse text-center mt-4">
-                    مسافر گرامی، صفحه مورد نظر شما یافت نشد.
+                    {
+                        props.title ? props.title :' مسافر گرامی، صفحه مورد نظر شما یافت نشد.'
+                    }
                 </h5>
                 <Link href="/" className="font-bold-iransanse text-center mt-4">
                     بازگشت به صفحه اصلی
