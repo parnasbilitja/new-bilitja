98f2adda3466d0c2a298ef57bcf0cf3e0c26a731
Pouya-A <58390637+Pouya-A@users.noreply.github.com>
2024-12-08 09:52:56 +0330

    filtered package by stars and price has been set

--- src/pages/[CityTour].js
+++ src/pages/[CityTour].js
@@ -228,7 +228,7 @@ const CityTour = (props) => {
 
         let data = await axios.get('https://api.hotelobilit.com/api/v2/tours/destinations', {
             headers: {
-                "x-app-key": '498|dNk7pOSiwfVlyX6uNWejkZ136Oy9U5iJTpne87PP' //the token is a variable which holds the token
+                "x-app-key": '1671|4fd32tDjR5YMiFBuPTIiRHJhDkKgGrd5SaBigR6C5a86ac05' //the token is a variable which holds the token
             }
         }, {
             destination: props.cityInfo.code
