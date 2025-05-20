10dc0821cc24adc5f6805d13736aec759615c348
Pouya-A <58390637+Pouya-A@users.noreply.github.com>
2025-05-20 12:22:21 +0330

    some changes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,18 +7,18 @@ export default class MyDocument extends Document {
         <Head>
 
 
-            <Script
-                dangerouslySetInnerHTML={{
-                    __html: `
-             window.dataLayer = window.dataLayer || [];
-             function gtag(){dataLayer.push(arguments);}
-             gtag('js', new Date());
-             gtag('config', 'GTM-WN4X4CF7', {
-               page_path: window.location.pathname,
-             });
-           `,
-                }}
-            />
+           {/* <Script*/}
+           {/*     dangerouslySetInnerHTML={{*/}
+           {/*         __html: `*/}
+           {/*  window.dataLayer = window.dataLayer || [];*/}
+           {/*  function gtag(){dataLayer.push(arguments);}*/}
+           {/*  gtag('js', new Date());*/}
+           {/*  gtag('config', 'GTM-WN4X4CF7', {*/}
+           {/*    page_path: window.location.pathname,*/}
+           {/*  });*/}
+           {/*`,*/}
+           {/*     }}*/}
+           {/* />*/}
 
 
             {/*{(process.env.NODE_ENV === 'production') &&*/}
